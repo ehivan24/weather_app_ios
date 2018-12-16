8bda2bafcdd5a3cd0ec02499c72ebf435eb0ca94
edwing santos <ehivan24@yahoo.com>
2018-12-16 17:28:23 -0500

    constrain-layout-for background.
    constrain-layout-for background.  not working on landscape mode

---
+++ Weather/Weather/BlueBackgroundView.swift
@@ -0,0 +1,22 @@
+//
+//  BlueBackgroundView.swift
+//  Weather
+//
+//  Created by edwing santos on 12/15/18.
+//  Copyright © 2018 edwing santos. All rights reserved.
+//
+
+import Foundation
+
+class BlueBackgroundView {
+    
+    // red: 95.5 / 255.0, green: 165.6 / 255.0, blue: 1.0, alpha: 1.0
+    static var redTopColor: Float = 95.5
+    static var greenTopColor: Float = 165.6
+    static var blueTopColor: Float = 1.0
+    
+    // red: 72 / 255.0, green: 114.0 / 255.0, blue: 184.0 / 255, alpha: 1.0
+    static var redBottomColor: Float = 72.0
+    static var greenBottomColor: Float = 114.0
+    static var blueBottomColor: Float = 184.0
+}
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mLocationLabel: UILabel!
+    @IBOutlet weak var mDayOfWeekLabel: UILabel!
+    @IBOutlet weak var mConditionImageView: UIImageView!
+    @IBOutlet weak var mConditionLabel: UILabel!
+    @IBOutlet weak var mConditionDegreesLabel: UILabel!
+    @IBOutlet weak var mBackgroundView: UIView!
+    
+    let mGradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mBackgroundView.layer.addSublayer(mGradientLayer)
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setViewBackground(redTopColor: BlueBackgroundView.redTopColor,
+                          greenTopColor: BlueBackgroundView.greenTopColor,
+                          blueTopColor: BlueBackgroundView.blueTopColor,
+                          redBottomColor: BlueBackgroundView.redBottomColor,
+                          greenBottomColor: BlueBackgroundView.blueBottomColor,
+                          blueBottomColor: BlueBackgroundView.blueBottomColor, darkBackground: false)
+    }
+    
+    func setViewBackground(redTopColor: Float, greenTopColor: Float, blueTopColor: Float,
+                           redBottomColor: Float, greenBottomColor: Float, blueBottomColor: Float, darkBackground: Bool) {
+        var bottomViewColor: CGColor!
+        var topViewColor: CGColor!
 
-
+        if darkBackground {
+            topViewColor = UIColor(red: CGFloat(redTopColor / 255.0),
+                                   green: CGFloat(greenTopColor / 255.0),
+                                   blue: CGFloat(blueTopColor / 255.0),
+                                   alpha: 1.0).cgColor
+        } else {
+            topViewColor = UIColor(red: CGFloat(redTopColor / 255.0),
+                                   green: CGFloat(greenTopColor / 255.0),
+                                   blue: CGFloat(blueTopColor),
+                                   alpha: 1.0).cgColor
+        }
+        
+        bottomViewColor = UIColor(red: CGFloat(redBottomColor / 255.0),
+                                      green: CGFloat(redBottomColor / 255.0),
+                                      blue: CGFloat(redBottomColor / 255.0), alpha: 1.0).cgColor
+        
+        mGradientLayer.frame = view.bounds
+        mGradientLayer.colors = [topViewColor, bottomViewColor]
+    }
 }
 
